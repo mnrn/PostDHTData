352f96a1f8aa86a3b20fa0f3c0957dda7ee548f4
ayase <paper.airplane01@gmail.com>
2019-02-24 00:01:18 +0900

    Implement post.

--- PostDHTData.ino
+++ PostDHTData.ino
@@ -1,13 +1,24 @@
 #include <ArduinoJson.h>
 #include <DHT.h>
+#include <Ethernet.h>
+#include <SPI.h>
 
 constexpr uint8_t dhtPin = 2;       // Pin connected to the DHT sensor.
 constexpr uint8_t dhtType = DHT22;  // Use DHT 22 (AM2302)
 DHT dht{dhtPin, dhtType};
 
 void setup() {
+  // Initialize Serial port.
   Serial.begin(9600);
+
+  // Initialize DHT.
   dht.begin();
+
+  // Initialize Ethernet library
+  byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+  if (!Ethernet.begin(mac)) {
+    Serial.println(F("Failed to configure Ethernet"));
+  }
 }
 
 void loop() {
@@ -46,6 +57,26 @@ void loop() {
   // Serialize Json.
   root.printTo(Serial);
   Serial.print("\n");  // Delimiter to use.
+
+  // Http request.
+  EthernetClient client;
+  client.setTimeout(2000);
+  if (client.connect("192.168.1.6", 2888)) {
+    Serial.println("Connected to server.");
+    client.println("POST /dht HTTP/1.1");
+    client.println("Host: 192.168.1.6");
+    client.println("Content-Type: application/json");
+    client.print("Content-Length: ");
+    client.println(root.measureLength());
+    client.println();
+    root.printTo(client);
+
+    client.flush();
+    client.stop();
+  }
+  else {
+    Serial.println("Failed to connected to server.");
+  }
 }
 
 // Sets the error value at errors key.
